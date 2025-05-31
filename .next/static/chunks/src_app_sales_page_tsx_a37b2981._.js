86512dd61abf29fa260b3c14c44566bd8e830648
somankd <soman.khadka@yahoo.com>
2025-05-31 21:15:39 +0545

    Add .next/static/chunks/src_app_sales_page_tsx_a37b2981._.js 1748705438537

---
+++ .next/static/chunks/src_app_sales_page_tsx_a37b2981._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_24211c3a._.js",
+  "static/chunks/src_13f1a2bc._.js"
+],
+    source: "dynamic"
+});
